244a6cbc8ff85a2c0ae19f383a04417ccdef0840
Tanmay Srivastava <fromtanmay@gmail.com>
2023-01-28 00:55:47 +0530

    final commit

---
+++ ATMinterface.java
@@ -0,0 +1,8 @@
+package ATM;
+
+public interface ATMinterface {
+    public void viewBalance();
+    public void withdrawAmount(double withdrawAmount);
+    public void depositAmount(double depositAmount );
+    public  void viewMiniStatement();
+}
